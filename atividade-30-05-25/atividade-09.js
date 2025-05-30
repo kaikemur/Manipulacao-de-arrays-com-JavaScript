7975d83a6c8068af09bc193718072530125dc76f
kaike <kaike.ribeiro@aluno.senai.br>
2025-05-30 15:34:44 -0300

    finaliza arquivo atividade-09

---
+++ atividade-30-05-25/atividade-09.js
@@ -0,0 +1,12 @@
+/*9 – Conferência de presença
+O RH quer saber se o funcionário "Amanda Souza" compareceu à reunião de alinhamento. Verifique essa informação na lista de presença*/
+
+let presenca = [
+  "João Lima",
+  "Carla Nunes",
+  "Amanda Souza",
+  "Bruno Ribeiro",
+  "Fernanda Sales"
+];
+
+console.log("A conferençia de presença a Amanda Souza compareceu`?" , presenca.includes(`Amanda Souza`));
