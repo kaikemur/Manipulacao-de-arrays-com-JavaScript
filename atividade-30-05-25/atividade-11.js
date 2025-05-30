b64b2a05870c1aab4e855adc91021b8628924282
kaike <kaike.ribeiro@aluno.senai.br>
2025-05-30 15:44:00 -0300

    finaliza arquivo atividade-11

---
+++ atividade-30-05-25/atividade-11.js
@@ -0,0 +1,14 @@
+/*11 – Geração de frase
+Você precisa exibir os ingredientes de uma receita em uma única frase legível para o cliente. 
+Utilize o array de ingredientes.
+*/
+let ingredientes = [
+  "farinha de trigo",
+  "fermento",
+  "leite",
+  "ovos",
+  "açúcar",
+  "manteiga"
+];
+
+console.log(`lista de ingredientes  : ${ingredientes.join(`, `)}`)
