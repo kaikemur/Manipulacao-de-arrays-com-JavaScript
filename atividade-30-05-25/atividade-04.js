19f76822ca821ab16f9a603c4d3b1a9db6e3ac24
Kaike <kaike.ribeiro@aluno.senai.br>
2025-05-30 11:36:39 -0300

    finaliza arquivo atividade-04

---
+++ atividade-30-05-25/atividade-04.js
@@ -0,0 +1,9 @@
+/* 4 – Cancelamento de última inscrição
+Um aluno se inscreveu por engano em um minicurso da plataforma e pediu para cancelar. Remova a última inscrição da lista de interessados.*/
+let inscritos = [
+  "Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"
+];
+
+inscritos.pop(``)
+
+console.log(`sem a pessoa que se inscreveu sem querer:`,inscritos);
