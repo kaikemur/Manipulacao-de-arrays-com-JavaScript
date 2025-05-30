6a85b2cc8166de8723ca90ab30466c54182608f9
kaike <kaike.ribeiro@aluno.senai.br>
2025-05-30 15:53:49 -0300

    finaliza arquivo atividade-13

---
+++ atividade-30-05-25/atividade-13.js
@@ -0,0 +1,15 @@
+/* 13 – Relatório de férias: Primeira semana
+Você precisa gerar um relatório com apenas a primeira semana de férias dos colaboradores listados,
+ sem modificar a lista original.*/
+
+ let diasFaltas = [
+  "Carlos – Segunda",
+  "Carlos – Terça",
+  "Carlos – Quarta",
+  "Carlos – Quinta",
+  "Carlos – Sexta",
+  "Carlos – Segunda (semana 2)",
+  "Carlos – Terça (semana 2)",
+  "Carlos – Quarta (semana 2)"
+];
+console.log("parte da parte de relatorio de ferias (indices 0 a 5):",diasFaltas.slice(0, 5));
