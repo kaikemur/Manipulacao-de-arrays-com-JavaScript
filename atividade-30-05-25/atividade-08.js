0cd9a6500c5b0d1b7e8e451c7485a1216dd4b474
Kaike <kaike.ribeiro@aluno.senai.br>
2025-05-30 11:52:12 -0300

    finaliza arquivo atividade-08

--- atividade-30-05-25/atividade-08.js
+++ atividade-30-05-25/atividade-08.js
@@ -1,3 +1,16 @@
 /* 8 – Alunos transferidos
 Duas alunas transferidas chegaram atrasadas e devem ser colocadas no início da lista de chamada. Os nomes são "Beatriz" e "Rafaela".
 */
+
+let chamada = [
+  "Carlos",
+  "Fernanda",
+  "João",
+  "Mariana",
+  "Lucas",
+  "Camila"
+];
+
+chamada.unshift(`Beatriz`,`Rafaela`)
+
+console.log(`fila de Alunos transferidos:`, chamada);
